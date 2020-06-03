4b93b32ac2ecfa0ac7207f3792bf71c3a2b8378b
Lakshman18 <42791037+Lakshman18@users.noreply.github.com>
2020-06-02 19:20:47 -0700

    Update views.py

--- covid/views.py
+++ covid/views.py
@@ -8,10 +8,19 @@ def index(request):
     url ='https://covid19.mathdro.id/api/'
     r = requests.get(url.format()).json()
 
+    x = r['confirmed']['value']
+    x1 = "{:,}".format(x)
+
+    y = r['recovered']['value']
+    y1 = "{:,}".format(y)
+
+    z = r['deaths']['value']
+    z1 = "{:,}".format(z)
+
     country_stats = {
-            'confirmed' : r['confirmed']['value'],
-            'recovered' : r['recovered']['value'],
-            'deaths' : r['deaths']['value'],
+            'confirmed' : x1,
+            'recovered' : y1,
+            'deaths' : z1,
             'update' : r['lastUpdate'][:10] ,
         }
 
@@ -24,16 +33,25 @@ def index(request):
             url ='https://covid19.mathdro.id/api/'
             r = requests.get(url.format()).json()
 
+            x = r['confirmed']['value']
+            x1 = "{:,}".format(x)
+
+            y = r['recovered']['value']
+            y1 = "{:,}".format(y)
+
+            z = r['deaths']['value']
+            z1 = "{:,}".format(z)
+
             country_stats = {
-                'confirmed' : r['confirmed']['value'],
-                'recovered' : r['recovered']['value'],
-                'deaths' : r['deaths']['value'],
-                'update' : r['lastUpdate'][:10] ,
-            }
+                    'confirmed' : x1,
+                    'recovered' : y1,
+                    'deaths' : z1,
+                    'update' : r['lastUpdate'][:10] ,
+                }
 
             context = {"country_stats": country_stats}
             print(context)
-        
+
         else:
             c = request.POST['country']
             url ='https://covid19.mathdro.id/api/countries/{}'
@@ -41,13 +59,21 @@ def index(request):
 
             r = requests.get(url.format(country)).json()
 
+            x = r['confirmed']['value']
+            x1 = "{:,}".format(x)
+
+            y = r['recovered']['value']
+            y1 = "{:,}".format(y)
+
+            z = r['deaths']['value']
+            z1 = "{:,}".format(z)
+
             country_stats = {
-                'country' : c,
-                'confirmed' : r['confirmed']['value'],
-                'recovered' : r['recovered']['value'],
-                'deaths' : r['deaths']['value'],
-                'update' : r['lastUpdate'][:10] ,
-            }
+                    'confirmed' : x1,
+                    'recovered' : y1,
+                    'deaths' : z1,
+                    'update' : r['lastUpdate'][:10] ,
+                }
 
             context = {"country_stats": country_stats}
             print(context)
